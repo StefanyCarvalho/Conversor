7bd09cbe356f59b9cf1d46a7f03cd0b854096a50
Stefany Carvalho <jmenezes241@gmail.com>
2019-09-07 15:08:14 -0300

    Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,115 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
+const request = "https://api.hgbrasil.com/finance?key=5e6eb30e";
 
+void main() async {
+  // a função main agora é assincrona
+  runApp(MaterialApp(
+    home: Home(),
+    theme: ThemeData(
+        primaryColor: Colors.white,
+        cursorColor: Colors.black,
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black)))),
+  ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        title: Text("Conversor"),
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+          // ele vai construir a tela dependendo do que estiver no getData()
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text(
+                    "Carregando Dados...",
+                    style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      "Erro ao Carregar Dados",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                } else {
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(
+                          Icons.monetization_on,
+                          color: Colors.black,
+                          size: 120.0,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Reais",
+                              labelStyle: TextStyle(color: Colors.black),
+                              border: OutlineInputBorder(),
+                              prefixText: "R\$"),
+                          style: TextStyle(color: Colors.black, fontSize: 25.0),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Dolares",
+                              labelStyle: TextStyle(color: Colors.black),
+                              border: OutlineInputBorder(),
+                              prefixText: "US\$"),
+                          style: TextStyle(color: Colors.black, fontSize: 25.0),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Euros",
+                              labelStyle: TextStyle(color: Colors.black),
+                              border: OutlineInputBorder(),
+                              prefixText: "€"),
+                          style: TextStyle(color: Colors.black, fontSize: 25.0),
+                        )
+                      ],
+                    ),
+                  );
+                }
+                break;
+            }
+          }),
+    );
+  }
+}
